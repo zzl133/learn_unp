f5845cd3404b0500d2fc8fb1adf347f27bcf4fee
tianer <q2719833@126.com>
2015-03-29 18:48:33 +0800

    a hook eg

---
+++ hook_eg/nf_sockopte.h
@@ -0,0 +1,19 @@
+/*
+ * file:nf_sockopte.h 
+ * sockopt extern header file
+ *
+ */
+
+#ifndef _NF_SOCKOPTE_H__
+#define _NF_SOCKOPTE_H__
+
+/*cmd 命令定义
+ *SOE_BANDIP:IP地址发送禁止命令
+ *SOE_BANDPORT:端口禁止命令
+ *SOE_BANDPING:ping禁止
+ */
+#define SOE_BANDIP   0x6001
+#define SOE_BANDPORT 0x6002
+#define SOE_BANDPING 0x6003
+
+#endif //_NF_SOCKOPTE_H__
